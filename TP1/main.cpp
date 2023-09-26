25a1de7f54158c66495f8f80c577bf78d894f093
ZOGHLAMI Abdeljalil <81550632+SomeOne768@users.noreply.github.com>
2023-09-26 15:16:34 +0200

    rotation

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,49 +1,61 @@
 #include <iostream>
 
-
-
 int div1(int n);
 int div2(int n, int i);
 int mult(int n);
 int mult(int n, int k);
 unsigned int deplacementBit(unsigned int n, unsigned k);
 unsigned int decremente(unsigned int i);
+unsigned int incremente(unsigned int i);
+unsigned int deplacementBit(unsigned int n, unsigned int k);
+unsigned int deplacementBitV2(unsigned int n, unsigned int k);
+unsigned int deplacementBitV4(unsigned int n, unsigned int k);
 
 int main(){
 
 
     // 1.
-    std::cout << "size of:" << "\n";
-    std::cout << "int \t\t:" << sizeof(int) <<"\n";
-    std::cout << "unsigned int \t:" << sizeof(unsigned int) <<"\n";
-    std::cout << "short int \t:" << sizeof(short int) <<"\n";
-    std::cout << "float \t\t:" <<  sizeof(float) <<"\n";
-    std::cout << "double \t\t:" << sizeof(double) <<"\n";
+    // std::cout << "size of:" << "\n";
+    // std::cout << "int \t\t:" << sizeof(int) <<"\n";
+    // std::cout << "unsigned int \t:" << sizeof(unsigned int) <<"\n";
+    // std::cout << "short int \t:" << sizeof(short int) <<"\n";
+    // std::cout << "float \t\t:" <<  sizeof(float) <<"\n";
+    // std::cout << "double \t\t:" << sizeof(double) <<"\n";
 
-    // 2.
-    int val1 = 435435,
-        val2 = 324436493;
+    // // 2.
+    // int val1 = 435435,
+    //     val2 = 324436493;
     
-    std::cout<< "au départ:\n";
-    std::cout<<"val1: "<< val1 << "\t val2: " << val2 << std::endl;
+    // std::cout<< "au départ:\n";
+    // std::cout<<"val1: "<< val1 << "\t val2: " << val2 << std::endl;
 
-    // Permutation de valeur
-    val1 += val2;
-    val2 = val1 -val2;
-    val1 -= val2;
+    // // Permutation de valeur
+    // val1 += val2;
+    // val2 = val1 -val2;
+    // val1 -= val2;
 
-    std::cout<< "ensuite:\n";
-    std::cout<<"val1: "<< val1 << "\t val2: " << val2 << std::endl;
+    // std::cout<< "ensuite:\n";
+    // std::cout<<"val1: "<< val1 << "\t val2: " << val2 << std::endl;
 
-    // 3.
+    // // 3.
 
-    std::cout << "\n\nQuestion 3\n" << "Division de 2 par 2: " << div1(2);
-    std::cout << "\n\nQuestion 4\n" << "Division de 8 par 2²: " << div2(8, 2);
-    std::cout << "\n\nQuestion 5\n" << "Multiplication de 8 par 2: " << mult(8);
-    std::cout << "\n\nQuestion 5-2\n" << "Multiplication de 8 par 32: " << mult(8, 32);
+    // std::cout << "\n\nQuestion 3\n" << "Division de 2 par 2: " << div1(2);
+    // std::cout << "\n\nQuestion 4\n" << "Division de 8 par 2²: " << div2(8, 2);
+    // std::cout << "\n\nQuestion 5\n" << "Multiplication de 8 par 2: " << mult(8);
+    // std::cout << "\n\nQuestion 5-2\n" << "Multiplication de 8 par 32: " << mult(8, 32);
 
-    // 6.
-    std::cout << "\n\nQuestion 6\n" << "test: " << decremente(10);
+    // // 6.
+    // std::cout << "\n\nQuestion 6\n" << "test: " << deplacementBit(10, 2) << "\n";
+
+    printf("%x %x\n", 1, deplacementBitV4(0x1, 5));
+
+    // //TEST
+    // unsigned int test = 0;
+    // while(test < 10)
+    // {
+    //     std::cout<< "test: " << test << std::endl;
+    //     test = incremente(test);
+    // }
 
     std::cout<<"\n";
     return 0;
@@ -82,18 +94,38 @@ int mult(int n, int k)
     return n;
 }
 
-void incremente(unsigned int &i)
+int find1(unsigned int n)
 {
-    if(i & 1)
+
+}
+
+unsigned int incremente(unsigned int i)
+{
+    if(! (i & 1) )
     {
-        // le dernier bit est un 1
-        i >> 1;
-        i << 1; // le dernier bit est devenu un 0
-        i << 1; // i a été incrémenté de 1
-    }
-    else{
+        // le dernier bit est un 0
         i = i | 1;
     }
+    else{
+        // il faut trouver le 1er bit à 0
+        unsigned int count = 0;
+        while( i&1 )
+        {
+            i>>1;
+            if(count&1)
+            {
+                // count est pair
+                count = count | 1;
+            }
+            else
+            {
+
+            }
+        }
+        
+    }
+
+    return i;
 }
 
 unsigned int deplacementBit(unsigned int n, unsigned int k)
@@ -102,17 +134,90 @@ unsigned int deplacementBit(unsigned int n, unsigned int k)
     // k: décalage à effectuer k € [0; 31]
 
     // On récupere les k plus petits bits + decalage à droite (>>)
-    unsigned int    dernier_bit = 0,
-                    res = 0,
-                    max = 1 << 32;
+    unsigned int    dernier_bit,
+                    res = 0;
+
     while(k != 0){
         dernier_bit = 1 & n;
-        n >> 1;
+        dernier_bit = dernier_bit << 31;
+        n = n >> 1;
         res = res >> 1;
-        res = (max & (dernier_bit<<32)) | res;
+        res = dernier_bit | res;
+
+        k--;
     }
 
-    // On decale ces bits de 32 - k vers la gauche (<<)
-    // On fait un | car normalement le premiers sont devenus des 0
+    // res contient les bits supprimés de n, au bon emplacement
 
+    // on recupère les bits restants de n
+    res = res | n;
+
+    return res;
+}
+
+
+unsigned int deplacementBitV2(unsigned int n, unsigned int k)
+{
+    // n: vecteur booléen
+    // k: décalage à effectuer k € [0; 31]
+
+    // On récupere les k plus petits bits + decalage à droite (>>)
+    unsigned int mask = 0,
+                 i = 0;
+
+    
+    // On crée un masque
+    while(i != k){
+        mask = mask<<1;
+        mask++;
+        i++;
+        std::cout << "mask: " << mask << "\n";
+    }
+
+    // On recupère les bits
+    unsigned int res = mask & n;
+
+    // On decale
+    res = res << (31-k);
+    n = n >> (k); 
+
+    std::cout << "res: " << res << "\nn: " << n << "\n";
+
+    // On combine les différents nombre
+    res = res | n;
+
+    return res;
+}
+
+
+unsigned int deplacementBitV3(unsigned int n, unsigned int k)
+{
+    // n: vecteur booléen
+    // k: décalage à effectuer k € [0; 31]
+
+    // On récupere les k plus petits bits + decalage à droite (>>)
+    unsigned int mask;
+
+    // On crée un masque
+    mask = 0xFFFFFFFF << k;
+    mask = 0xFFFFFFFF ^ mask;
+
+    // On recupère les bits
+    unsigned int res = mask & n;
+
+    // On decale
+    res = res << (31-k);
+    n = n >> (k); 
+
+    std::cout << "res: " << res << "\nn: " << n << "\n";
+
+    // On combine les différents nombre
+    res = res | n;
+
+    return res;
+}
+
+unsigned int deplacementBitV4(unsigned int n, unsigned int k)
+{
+    return (n << (31-k)) | (n>>k);
 }
