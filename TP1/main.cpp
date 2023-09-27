d46a7df6961f3e43ad02966bb55316f3f61996d8
SomeOne768 <jalil0169@gmail.com>
2023-09-27 22:49:44 +0200

    matrice

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bitset>
+#include <cstdlib>
 
 int div1(int n);
 int div2(int n, int i);
@@ -17,9 +18,17 @@ void switchValue(T &v1, T &v2);
 template <typename T>
 void switchValueXor(T &v1, T &v2);
 unsigned int inversion(unsigned int n);
+template <typename T>
+void fillTab(T tab[][5], unsigned int nb_lignes, unsigned int nb_colonnes);
+template <typename T>
+void fillTab(T tab[], unsigned int nb_cases);
+template <typename T>
+void afficheTab(T tab[][5], unsigned int nb_lignes, unsigned int nb_colonnes);
+template <typename T>
+void afficheTab(T tab[], unsigned int nb_cases);
 
-int main(){
-
+int main()
+{
 
     // 1.
     // std::cout << "size of:" << "\n";
@@ -32,7 +41,7 @@ int main(){
     // 2.
     // int val1 = 435435,
     //     val2 = 324436493;
-    
+
     // std::cout<< "au départ:\n";
     // std::cout<<"val1: "<< val1 << "\t val2: " << val2 << std::endl;
 
@@ -56,10 +65,33 @@ int main(){
     // printf("\nQuestion 6\nDeplacement de 0x%x de %d bits : 0x%x\n", val, offset, deplacementBitV4(val, offset));
 
     // 7
-    std::bitset<sizeof(unsigned int) *8> nombre_binaire(42);
-    std::cout << "nQuestion 7\nInversion de " << nombre_binaire << " : " <<  std::bitset<sizeof(unsigned int) *8>(inversion(42)) << "\n";
+    // std::bitset<sizeof(unsigned int) *8> nombre_binaire(42);
+    // std::cout << "nQuestion 7\nInversion de " << nombre_binaire << " : " <<  std::bitset<sizeof(unsigned int) *8>(inversion(42)) << "\n";
 
+    // Multiplication matrice/vecteur binaires.
+
+    // 8
+    unsigned int t1[5][5],
+        t2[25],
+        **t3 = new unsigned int *[5];
     
+    
+    
+    // Libération mémoire
+    for (auto i = 0; i < 5; i++)
+        t3[i] = new unsigned int[5];
+
+    for (auto i = 0; i < 5; i++)
+        delete[] t3[i];
+    delete[] t3;
+
+    // 9
+
+    fillTab(t1, 5, 5);
+    fillTab(t2, 25);
+
+    afficheTab(t1, 5, 5);
+    afficheTab(t2, 25);
     // //TEST
     // unsigned int test = 0;
     // while(test < 10)
@@ -68,10 +100,9 @@ int main(){
     //     test = incremente(test);
     // }
 
-    std::cout<<"\n";
+    std::cout << "\n";
     return 0;
 }
-
 
 template <typename T>
 void switchValueXor(T &v1, T &v2)
@@ -81,7 +112,6 @@ void switchValueXor(T &v1, T &v2)
     v1 = v1 ^ v2;
 }
 
-
 template <typename T>
 void switchValue(T &v1, T &v2)
 {
@@ -90,16 +120,16 @@ void switchValue(T &v1, T &v2)
     v1 = v1 - v2;
 }
 
-
-int div1(int n){
+int div1(int n)
+{
     // n: entier positif pair
-    return n>>1;
+    return n >> 1;
 }
 
 int div2(int n, int i)
 {
     // n: entier positif pair
-    // i: entier 
+    // i: entier
     return n >> i;
 }
 
@@ -109,13 +139,13 @@ unsigned int div3(unsigned int n, unsigned int i)
     // i: entier positif
     // int reste = 0xFFFFFFFF >> (32 - i);
     // return reste & n;
-    return (n << (32-i)) >> (32 -i);
+    return (n << (32 - i)) >> (32 - i);
 }
 
 int mult(int n)
 {
     // n entier positif
-    return n<<1;
+    return n << 1;
 }
 
 unsigned int mult(unsigned int n, unsigned int k)
@@ -123,15 +153,15 @@ unsigned int mult(unsigned int n, unsigned int k)
     // n entier positif par
     // k nombre creux
 
-    // On va decomposer k en une somme de puissance de 2 
+    // On va decomposer k en une somme de puissance de 2
     // Ex: k = 6 et n = 2 => k = 0110 et n = 0010
     // On fait n * ( 4 + 2) ce qui revient à faire n * (2² + 2¹)
     // de cette manière on pourra utiliser l'opérateur >> et << pour multiplier
     unsigned int somme = 0,
                  puissance_2 = 0;
-    while(k)
+    while (k)
     {
-        if(k & 1)
+        if (k & 1)
         {
             somme += n << puissance_2;
         }
@@ -143,17 +173,17 @@ unsigned int mult(unsigned int n, unsigned int k)
     return somme;
 }
 
-
 unsigned int deplacementBit(unsigned int n, unsigned int k)
 {
     // n: vecteur booléen
     // k: décalage à effectuer k € [0; 31]
 
     // On récupere les k plus petits bits + decalage à droite (>>)
-    unsigned int    dernier_bit,
-                    res = 0;
+    unsigned int dernier_bit,
+        res = 0;
 
-    while(k != 0){
+    while (k != 0)
+    {
         dernier_bit = 1 & n;
         dernier_bit = dernier_bit << 31;
         n = n >> 1;
@@ -171,7 +201,6 @@ unsigned int deplacementBit(unsigned int n, unsigned int k)
     return res;
 }
 
-
 unsigned int deplacementBitV2(unsigned int n, unsigned int k)
 {
     // n: vecteur booléen
@@ -181,10 +210,10 @@ unsigned int deplacementBitV2(unsigned int n, unsigned int k)
     unsigned int mask = 0,
                  i = 0;
 
-    
     // On crée un masque
-    while(i != k){
-        mask = mask<<1;
+    while (i != k)
+    {
+        mask = mask << 1;
         mask++;
         i++;
         std::cout << "mask: " << mask << "\n";
@@ -194,8 +223,8 @@ unsigned int deplacementBitV2(unsigned int n, unsigned int k)
     unsigned int res = mask & n;
 
     // On decale
-    res = res << (31-k);
-    n = n >> (k); 
+    res = res << (31 - k);
+    n = n >> (k);
 
     std::cout << "res: " << res << "\nn: " << n << "\n";
 
@@ -204,7 +233,6 @@ unsigned int deplacementBitV2(unsigned int n, unsigned int k)
 
     return res;
 }
-
 
 unsigned int deplacementBitV3(unsigned int n, unsigned int k)
 {
@@ -222,8 +250,8 @@ unsigned int deplacementBitV3(unsigned int n, unsigned int k)
     unsigned int res = mask & n;
 
     // On decale
-    res = res << (31-k);
-    n = n >> (k); 
+    res = res << (31 - k);
+    n = n >> (k);
 
     std::cout << "res: " << res << "\nn: " << n << "\n";
 
@@ -235,10 +263,57 @@ unsigned int deplacementBitV3(unsigned int n, unsigned int k)
 
 unsigned int deplacementBitV4(unsigned int n, unsigned int k)
 {
-    return (n << (32-k)) | (n>>k);
+    return (n << (32 - k)) | (n >> k);
 }
 
 unsigned int inversion(unsigned int n)
 {
-    return n ^ 0xFFFFFFFF;  
+    return n ^ 0xFFFFFFFF;
+}
+
+// Écrire des fonctions permettant de remplir les objets avec des 0 et des 1 de façon aléatoire.
+template <typename T>
+void fillTab(T tab[][5], unsigned int nb_lignes, unsigned int nb_colonnes)
+{
+    for (unsigned int i = 0; i < nb_lignes; i++)
+    {
+        for (unsigned int j = 0; j < nb_colonnes; j++)
+        {
+            tab[i][j] = rand() % 2;
+        }
+    }
+}
+
+template <typename T>
+void fillTab(T tab[], unsigned int nb_cases)
+{
+    for (unsigned int i = 0; i < nb_cases; i++)
+    {
+        tab[i] = rand() % 2;
+    }
+}
+
+template <typename T>
+void afficheTab(T tab[][5], unsigned int nb_lignes, unsigned int nb_colonnes)
+{
+    std::cout << "Tab:\n";
+    for (unsigned int i = 0; i < nb_lignes; i++)
+    {
+        for (unsigned int j = 0; j < nb_colonnes; j++)
+        {
+            std::cout << tab[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+template <typename T>
+void afficheTab(T tab[], unsigned int nb_cases)
+{
+    std::cout << "Tab:\n";
+    for (unsigned int i = 0; i < nb_cases; i++)
+    {
+        std::cout << tab[i] << " ";
+    }
+    std::cout << "\n";
 }
