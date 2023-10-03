27c53b40afa3b6ed8ad7fa8eaee05991f7d27d1a
remi-droid <giraudon.remi@gmail.com>
2023-10-03 14:50:01 +0200

    Encrypt vigenere

---
+++ TP2/main_cipher.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vigenere
+{
+  public:
+    string key;
+  
+    Vigenere(string key)
+    {
+      // Modifying all characters other than uppercase : lowercase -> uppercase, other -> delete
+      for(unsigned int i = 0; i < key.size(); ++i)
+      {
+        if(key[i] >= 'A' && key[i] <= 'Z')
+          this->key += key[i];
+        else if(key[i] >= 'a' && key[i] <= 'z')
+          this->key += key[i] + 'A' - 'a';
+      }
+    }
+  
+    string encrypt(string text)
+    {
+      string out;
+      int indexKey = 0;
+
+      // Modifying all characters other than uppercase : lowercase -> uppercase, other -> delete
+      for(unsigned int i = 0; i < text.length(); ++i)
+      {
+        if(text[i] >= 'A' && text[i] <= 'Z'){
+          out += ((text[i] - 'A' + key[indexKey] - 'A') % 26) + 'A';
+          indexKey = (indexKey + 1) % key.length();
+        }
+        else if(text[i] >= 'a' && text[i] <= 'z')
+        {
+          out += ((text[i] - 'a' + key[indexKey] - 'A') % 26) + 'A';
+          indexKey = (indexKey + 1) % key.length();
+        }
+      }
+
+      return out;
+    }
+  
+    string decrypt(string text)
+    {
+      string out;
+
+      // TO REMOVE
+      out = text;
+
+      // ADD THE VIGENERE DECRYPTION 
+      return out;
+    }
+};
+
+//////////////////////////////////////////////////////////////////
+//                             MAIN                             //
+//////////////////////////////////////////////////////////////////
+
+int main()
+{
+  Vigenere cipher("algorythme");
+ 
+  string original_en  = "pythagore";
+  string encrypted_en = cipher.encrypt(original_en);
+  string decrypted_en = cipher.decrypt(encrypted_en);
+ 
+  cout << original_en << endl;
+  cout << "Encrypted: " << encrypted_en << endl;
+  cout << "Decrypted: " << decrypted_en << endl;
+
+  string original_fr  = "Principe de Kerckhoffs - Toute la securite d'un systeme cryptographique doit reposer sur la clef et pas sur le systeme lui meme.";
+
+  string encrypted_fr = cipher.encrypt(original_fr);
+  string decrypted_fr = cipher.decrypt(encrypted_fr);
+ 
+  cout << original_fr << endl;
+  cout << "Encrypted: " << encrypted_fr << endl;
+  cout << "Decrypted: " << decrypted_fr << endl;
+
+}
+
