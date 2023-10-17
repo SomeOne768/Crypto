c205ae96bf877c0b5e1c1e9e052f43efa61052e6
ZOGHLAMI Abdeljalil <81550632+SomeOne768@users.noreply.github.com>
2023-10-17 10:51:57 +0200

    clean and comment vigenere exo 1 and 2

--- TP2/Squelette de Code/main_cipher.cpp
+++ TP2/Squelette de Code/main_cipher.cpp
@@ -18,12 +18,12 @@ int main()
   cout << "Encrypted: " << encrypted_en << endl;
   cout << "Decrypted: " << decrypted_en << endl;
 
-  // string original_fr  = "Principe de Kerckhoffs - Toute la securite d'un systeme cryptographique doit reposer sur la clef et pas sur le systeme lui meme.";
+  string original_fr  = "Principe de Kerckhoffs - Toute la securite d'un systeme cryptographique doit reposer sur la clef et pas sur le systeme lui meme.";
 
-  // string encrypted_fr = cipher.encrypt(original_fr);
-  // string decrypted_fr = cipher.decrypt(encrypted_fr);
+  string encrypted_fr = cipher.encrypt(original_fr);
+  string decrypted_fr = cipher.decrypt(encrypted_fr);
 
-  // cout << original_fr << endl;
-  // cout << "Encrypted: " << encrypted_fr << endl;
-  // cout << "Decrypted: " << decrypted_fr << endl;
+  cout << original_fr << endl;
+  cout << "Encrypted: " << encrypted_fr << endl;
+  cout << "Decrypted: " << decrypted_fr << endl;
 }
