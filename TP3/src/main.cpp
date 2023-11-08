19a24bb6c9c1dfb6973eea120868680fabb8d725
abzoghlami <abzoghlami@a112pc15L.local.isima.fr>
2023-11-08 15:09:58 +0100

    adding ShiftRow

--- TP3/src/main.cpp
+++ TP3/src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 void buildInversor(P16 tab[]);
+void shiftRow(P16 &);
 
 int main() {
   P16 tab[16];
@@ -20,8 +21,16 @@ void buildInversor(P16 tab[]) {
     while ((polynomeInverse * polynomeToFindInverse).polynome != 1) {
       polynomeInverse.multiplyX();
     }
-    
+
     tab[i] = polynomeInverse;
     polynomeToFindInverse.polynome <<= 1;
   }
+}
+
+
+void shiftRow(P16 &p, unsigned int i)
+{
+    short int lastBits = p.polynome >> (16-i);
+    p.polynome <<= i;
+    p.polynome += lastBits;
 }
